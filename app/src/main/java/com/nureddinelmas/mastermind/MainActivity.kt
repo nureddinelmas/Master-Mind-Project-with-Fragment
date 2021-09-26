47acfcd7e10a82c4a6c1bc9ac10a8f0140e0fa42
Nureddin Elmas <nureddinelmas34@gmail.com>
2021-09-26 23:14:47 +0200

    Master Mind Project with Fragment started

---
+++ app/src/main/java/com/nureddinelmas/mastermind/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nureddinelmas.mastermind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
